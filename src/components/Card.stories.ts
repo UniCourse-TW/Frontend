0f22c5b32138f1b128c685e8bf9a967397216755
TzuWei <ap9940506@gmail.com>
2022-02-07 15:45:43 +0800

    refactor: Using proper example in Card story

--- src/components/Card.stories.ts
+++ src/components/Card.stories.ts
@@ -17,7 +17,19 @@ type T = typeof Template & { args?: unknown };
 
 export const normal = Template.bind({}) as T;
 normal.args = {
-    default: `<div class="py-2 text-lg font-bold">早上好中國</div>`,
+    default: `
+    <div class="flex h-full flex-col gap-y-2">
+        <div class="flex items-center gap-x-2">
+            <img src="https://picsum.photos/seed/1/32/32" class="rounded-full" />
+            <span class="text-lg font-bold">大推王老師程設一</span>
+        </div>
+        <div class="text-gray-600 text-sm">課程：110-1 程式設計（一）</div>
+
+        <div class="flex-1"></div>
+
+        <div class="text-gray-700">#心得</div>
+        <div class="text-gray-700 text-sm">2022/02/07 15:38</div>
+    </div>`,
 };
 
 export const featured = Template.bind({}) as T;
@@ -27,6 +39,18 @@ featured.args = {
 
 export const custom_classes = Template.bind({}) as T;
 custom_classes.args = {
-    class: "h-20 text-center w-1/2",
-    default: `<div class="py-2 text-lg font-bold">早上好中國</div>`,
+    class: "cursor-pointer w-1/3 py-4",
+    default: `
+    <div class="flex h-full flex-col gap-y-2">
+        <div class="flex items-center gap-x-2">
+            <img src="https://picsum.photos/seed/1/32/32" class="rounded-full" />
+            <span class="text-lg font-bold">大推王老師程設一</span>
+        </div>
+        <div class="text-gray-600 text-sm">課程：110-1 程式設計（一）</div>
+
+        <div class="flex-1"></div>
+
+        <div class="text-gray-700">#心得</div>
+        <div class="text-gray-700 text-sm">2022/02/07 15:38</div>
+    </div>`,
 };
