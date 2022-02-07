7450e63cf4295e49dc7fba8ca7801c8b528201e6
JacobLinCool <jacoblincool@gmail.com>
2022-02-08 04:50:00 +0800

    feat: add animation & icon components

---
+++ src/components/Icon/IconStars.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps({
+    max: { default: 5 },
+    value: { default: 0 },
+});
+</script>
+
+<template>
+    <i-octicon-star-fill-16
+        v-for="idx in props.max"
+        :key="idx"
+        :class="['inline', '-translate-y-0.5', idx <= props.value + 0.5 ? 'text-yellow-400' : 'text-gray-200']"
+    />
+</template>
