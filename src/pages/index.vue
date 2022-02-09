cdedbbac08be8acde6a3aa1855ccf4418a92359a
antingchao <98510877+antingchao@users.noreply.github.com>
2022-02-09 15:52:37 +0800

    adjust front page (#19)
    Co-authored-by: JacobLinCool <jacoblincool@gmail.com>

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,89 +1,103 @@
 <script lang="ts" setup>
 useHead({ title: "UniCourse" });
+
+function open_external(url: string) {
+    const win = window.open("", "_blank") as Window;
+    win.location.href = url;
+}
 </script>
 
 <template>
-    <div id="cover" class="absolute top-0 h-screen w-screen">
-        <img src="../assets/banner.svg" class="h-1/5 w-full sm:h-1/4 lg:h-1/3" />
-        <div style="margin-top: -50px; text-align: center; color: #8242b5; font-weight: bold">選課資訊交流平台</div>
-        <div class="mx-auto h-2/5 w-full p-2 sm:w-full sm:px-4 lg:w-2/3 lg:px-6">
-            <div class="inline-block h-full w-full pb-1 sm:h-full sm:w-1/2 sm:pr-2 lg:h-full lg:w-1/2 lg:pr-2">
+    <div class="absolute top-0 bg-gradient-to-r from-blue-300 via-indigo-200 to-fuchsia-200 pt-16">
+        <div id="cover" class="top-0 m-auto w-screen lg:h-screen">
+            <img src="../assets/banner_withText.svg" class="m-auto w-2/3 p-3 sm:p-5 lg:p-20" />
+        </div>
+        <div class="mx-auto h-4/5 w-full p-2 sm:h-2/5 sm:px-4 lg:w-2/3 lg:px-6">
+            <div class="inline-block h-full w-1/2 pt-1 pr-2 sm:h-full sm:w-1/2 sm:pr-2 lg:h-full lg:w-1/2">
                 <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                    <RainbowText class="text-2xl">目前有100位<br />使用者<br /></RainbowText>
+                    <span class="text-lg text-purple-700">目前有</span>
+                    <span class="text-lg text-purple-700 sm:text-3xl"> 100 </span>
+                    <span class="text-lg text-purple-700">位<br />使用者</span>
                     <img src="../assets/front/front0L.svg" class="inline-block h-1/2" />
                 </div>
             </div>
-            <div class="inline-block h-full w-full pt-1 sm:h-full sm:w-1/2 sm:pl-2 lg:h-full lg:w-1/2 lg:pl-2">
+
+            <div class="inline-block h-full w-1/2 pt-1 pl-2 sm:h-full sm:w-1/2 sm:pl-2 lg:h-full lg:w-1/2">
                 <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                    <RainbowText class="text-2xl">累計了100篇<br />文章<br /></RainbowText>
+                    <span class="text-lg text-purple-700">累計了</span>
+                    <span class="text-lg text-purple-700 sm:text-3xl"> 100 </span>
+                    <span class="text-lg text-purple-700">篇<br />文章</span>
                     <img src="../assets/front/front0R.svg" class="inline-block h-1/2" />
                 </div>
             </div>
         </div>
         <div class="mx-auto h-2/5 w-full p-2 sm:h-1/2 sm:px-4 lg:h-2/3 lg:w-2/3 lg:px-6">
-            <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                <img src="../assets/front/front01_transBG.svg" class="inline-block h-full w-2/5" />
-                <div class="inline-block h-full w-1/2">
-                    <div class="m-auto p-1 sm:p-5 lg:p-5">
-                        <font color="#8242B5">找到你有興趣的課程資訊</font>
+            <div
+                class="flex h-full w-full rounded-lg bg-white bg-opacity-40 px-3 py-6 text-center shadow-lg transition-all hover:shadow-xl"
+            >
+                <div class="inline-block h-full w-1/2 flex-1">
+                    <img src="../assets/front/front01_transBG.svg" class="my-auto inline-block w-full" />
+                </div>
+                <div class="my-auto inline-block h-full w-1/2 flex-1">
+                    <div class="m-auto p-3 sm:p-5 lg:p-5">
+                        <span class="text-lg text-purple-700">找到你有興趣的課程資訊</span>
                     </div>
-                    <input
-                        type="button"
-                        value="搜尋課程"
-                        onclick="location.href='course'"
-                        style="width: 120px; height: 40px; font-size: 15px; background-color: #8242b5; color: #fff; border-radius: 5px"
-                    />
+                    <router-link
+                        to="/course"
+                        class="h-10 w-32 rounded-md bg-purple-700 py-2.5 px-6 text-white transition-all hover:shadow-lg"
+                        >搜尋課程</router-link
+                    >
                 </div>
             </div>
         </div>
         <div class="mx-auto h-2/5 w-full p-2 sm:h-1/2 sm:px-4 lg:h-2/3 lg:w-2/3 lg:px-6">
-            <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                <img src="../assets/front/front02_transBG.svg" class="inline-block h-full w-2/5" />
-                <div class="inline-block h-full w-1/2">
-                    <div class="m-auto p-1 sm:p-5 lg:p-5">
+            <div
+                class="flex h-full w-full rounded-lg bg-white bg-opacity-40 px-3 py-6 text-center shadow-lg transition-all hover:shadow-xl"
+            >
+                <div class="inline-block h-full w-1/2 flex-1">
+                    <img src="../assets/front/front02_transBG.svg" class="my-auto inline-block w-full" />
+                </div>
+                <div class="my-auto inline-block h-full w-1/2 flex-1">
+                    <div class="m-auto p-3 sm:p-5 lg:p-5">
                         <font color="#8242B5">回答他人提出的課程相關問題</font>
                     </div>
-                    <input
-                        type="button"
-                        value="解答疑問"
-                        onclick="location.href='forum'"
-                        style="width: 120px; height: 40px; font-size: 15px; background-color: #8242b5; color: #fff; border-radius: 5px"
-                    />
+                    <router-link
+                        to="/course"
+                        class="h-10 w-32 rounded-md bg-purple-700 py-2.5 px-6 text-white transition-all hover:shadow-lg"
+                        >解答疑問</router-link
+                    >
                 </div>
             </div>
         </div>
         <div class="mx-auto h-2/5 w-full p-2 sm:h-1/2 sm:px-4 lg:h-2/3 lg:w-2/3 lg:px-6">
-            <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                <img src="../assets/front/front03_transBG.svg" class="inline-block h-full w-2/5" />
-                <div class="inline-block h-full w-1/2">
-                    <div class="m-auto p-1 sm:p-5 lg:p-5">
+            <div
+                class="flex h-full w-full rounded-lg bg-white bg-opacity-40 px-3 py-6 text-center shadow-lg transition-all hover:shadow-xl"
+            >
+                <div class="inline-block h-full w-1/2 flex-1">
+                    <img src="../assets/front/front03_transBG.svg" class="my-auto inline-block w-full" />
+                </div>
+                <div class="my-auto inline-block h-full w-1/2 flex-1">
+                    <div class="m-auto p-3 sm:p-5 lg:p-5">
                         <font color="#8242B5">和其他人分享你的修課心得</font>
                     </div>
-                    <input
-                        type="button"
-                        value="撰寫心得"
-                        onclick="location.href='forum'"
-                        style="width: 120px; height: 40px; font-size: 15px; background-color: #8242b5; color: #fff; border-radius: 5px"
-                    />
+                    <router-link
+                        to="/course"
+                        class="h-10 w-32 rounded-md bg-purple-700 py-2.5 px-6 text-white transition-all hover:shadow-lg"
+                        >撰寫心得</router-link
+                    >
                 </div>
             </div>
         </div>
         <div class="mx-auto h-1/4 w-full p-5 text-center lg:w-2/3">
             <font>加入我們 / find us<br /></font>
-            <div class="mx-auto w-1/5 p-3">
-                <button type="button" class="m-2 my-auto w-1/5" onclick="location.href='DC連結'">
-                    <img src="../assets/front/dc_icon/dc_icon_black.png" />
+            <div class="mx-auto w-1/2">
+                <button type="button" class="mx-auto w-1/4" @click="open_external('https://discord.gg/at3rvZ8QW7')">
+                    <img src="../assets/front/dc_icon/dc_icon_grey.svg" class="mx-auto" />
                 </button>
-                <button type="button" class="m-2 my-auto w-1/5" onclick="location.href='FB連結'">
-                    <img src="../assets/front/fb_icon/fb_icon_black.png" />
+                <button type="button" class="mx-auto hidden w-1/4" onclick="location.href='FB連結'">
+                    <img src="../assets/front/fb_icon/fb_icon_grey.svg" class="mx-auto" />
                 </button>
             </div>
         </div>
     </div>
 </template>
-
-<style scoped>
-/* .divBox {
-        background: green;
-    } */
-</style>
