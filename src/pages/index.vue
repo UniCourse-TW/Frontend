ea962f1353b12e06a196fbe00a894eb5e11e2cec
JacobLinCool <jacoblincool@gmail.com>
2022-02-10 16:03:36 +0800

    feat: improve SEO, meta description, img alt, external link rel, passive listener

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,24 +5,32 @@ useHead({ title: "UniCourse" });
 <template>
     <div class="absolute top-0 bg-gradient-to-r from-blue-300 via-indigo-200 to-fuchsia-200 pt-16">
         <div id="cover" class="top-0 m-auto w-screen lg:h-screen">
-            <img src="../assets/banner_withText.svg" class="m-auto w-2/3 p-3 sm:p-5 lg:p-20" />
+            <img alt="UniCourse Logo" src="../assets/banner_withText.svg" class="m-auto w-2/3 p-3 sm:p-5 lg:p-20" />
         </div>
         <div class="mx-auto h-4/5 w-full p-2 sm:h-2/5 sm:px-4 lg:w-2/3 lg:px-6">
             <div class="inline-block h-full w-1/2 pt-1 pr-2 sm:h-full sm:w-1/2 sm:pr-2 lg:h-full lg:w-1/2">
-                <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                    <span class="text-lg text-purple-700">目前有</span>
-                    <span class="text-lg text-purple-700 sm:text-3xl"> 100 </span>
-                    <span class="text-lg text-purple-700">位<br />使用者</span>
-                    <img src="../assets/front/front0L.svg" class="inline-block h-1/2" />
+                <div
+                    class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center text-lg text-purple-700 shadow-lg transition-all hover:shadow-xl"
+                >
+                    目前有
+                    <span class="sm:text-3xl"> {{ 100 }} </span>
+                    位
+                    <br />
+                    使用者
+                    <img alt="Community" src="../assets/front/front0L.svg" class="inline-block h-full" />
                 </div>
             </div>
 
             <div class="inline-block h-full w-1/2 pt-1 pl-2 sm:h-full sm:w-1/2 sm:pl-2 lg:h-full lg:w-1/2">
-                <div class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center shadow-lg transition-all hover:shadow-xl">
-                    <span class="text-lg text-purple-700">累計了</span>
-                    <span class="text-lg text-purple-700 sm:text-3xl"> 100 </span>
-                    <span class="text-lg text-purple-700">篇<br />文章</span>
-                    <img src="../assets/front/front0R.svg" class="inline-block h-1/2" />
+                <div
+                    class="h-full w-full rounded-lg bg-white bg-opacity-40 p-3 text-center text-lg text-purple-700 shadow-lg transition-all hover:shadow-xl"
+                >
+                    累計了
+                    <span class="sm:text-3xl"> {{ 100 }} </span>
+                    篇
+                    <br />
+                    文章
+                    <img alt="Articles" src="../assets/front/front0R.svg" class="inline-block h-full" />
                 </div>
             </div>
         </div>
@@ -31,11 +39,11 @@ useHead({ title: "UniCourse" });
                 class="flex h-full w-full rounded-lg bg-white bg-opacity-40 px-3 py-6 text-center shadow-lg transition-all hover:shadow-xl"
             >
                 <div class="inline-block h-full w-1/2 flex-1">
-                    <img src="../assets/front/front01_transBG.svg" class="my-auto inline-block w-full" />
+                    <img alt="Find Course" src="../assets/front/front01_transBG.svg" class="my-auto inline-block w-full" />
                 </div>
                 <div class="my-auto inline-block h-full w-1/2 flex-1">
                     <div class="m-auto p-3 sm:p-5 lg:p-5">
-                        <span class="text-lg text-purple-700">找到你有興趣的課程資訊</span>
+                        <span class="text-lg text-purple-700">找到你有興趣的<br class="sm:hidden" />課程資訊</span>
                     </div>
                     <Link to="/course" class="h-10 w-32 rounded-md bg-purple-700 py-2.5 px-6 text-white transition-all hover:shadow-lg"
                         >搜尋課程</Link
@@ -48,11 +56,11 @@ useHead({ title: "UniCourse" });
                 class="flex h-full w-full rounded-lg bg-white bg-opacity-40 px-3 py-6 text-center shadow-lg transition-all hover:shadow-xl"
             >
                 <div class="inline-block h-full w-1/2 flex-1">
-                    <img src="../assets/front/front02_transBG.svg" class="my-auto inline-block w-full" />
+                    <img alt="Answer Question" src="../assets/front/front02_transBG.svg" class="my-auto inline-block w-full" />
                 </div>
                 <div class="my-auto inline-block h-full w-1/2 flex-1">
                     <div class="m-auto p-3 sm:p-5 lg:p-5">
-                        <font color="#8242B5">回答他人提出的課程相關問題</font>
+                        <span class="text-lg text-purple-700">回答他人提出的<br class="sm:hidden" />課程相關問題</span>
                     </div>
                     <Link to="/course" class="h-10 w-32 rounded-md bg-purple-700 py-2.5 px-6 text-white transition-all hover:shadow-lg"
                         >解答疑問</Link
@@ -65,11 +73,11 @@ useHead({ title: "UniCourse" });
                 class="flex h-full w-full rounded-lg bg-white bg-opacity-40 px-3 py-6 text-center shadow-lg transition-all hover:shadow-xl"
             >
                 <div class="inline-block h-full w-1/2 flex-1">
-                    <img src="../assets/front/front03_transBG.svg" class="my-auto inline-block w-full" />
+                    <img alt="Share Review" src="../assets/front/front03_transBG.svg" class="my-auto inline-block w-full" />
                 </div>
                 <div class="my-auto inline-block h-full w-1/2 flex-1">
                     <div class="m-auto p-3 sm:p-5 lg:p-5">
-                        <font color="#8242B5">和其他人分享你的修課心得</font>
+                        <span class="text-lg text-purple-700">和其他人分享你的<br class="sm:hidden" />修課心得</span>
                     </div>
                     <Link to="/course" class="h-10 w-32 rounded-md bg-purple-700 py-2.5 px-6 text-white transition-all hover:shadow-lg"
                         >撰寫心得</Link
@@ -78,13 +86,11 @@ useHead({ title: "UniCourse" });
             </div>
         </div>
         <div class="mx-auto h-1/4 w-full p-5 text-center lg:w-2/3">
-            <font>加入我們 / find us<br /></font>
+            <span>加入我們 / find us</span>
+            <br />
             <div class="mx-auto w-1/2">
-                <Link class="mx-auto w-1/4" to="https://discord.gg/gMXEn97CkN" :icon="false">
-                    <img src="../assets/front/dc_icon/dc_icon_grey.svg" class="mx-auto" />
-                </Link>
-                <Link class="mx-auto hidden w-1/4" to="" :icon="false">
-                    <img src="../assets/front/fb_icon/fb_icon_grey.svg" class="mx-auto" />
+                <Link class="mx-auto w-1/4" aria-label="Join Our Discord Server" to="https://discord.gg/gMXEn97CkN" :icon="false">
+                    <img alt="Discord Icon" src="../assets/front/dc_icon/dc_icon_grey.svg" loading="lazy" class="mx-auto" />
                 </Link>
             </div>
         </div>
