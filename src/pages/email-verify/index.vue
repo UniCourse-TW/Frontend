b1b8aa1872bf555b7fcc8e82a438573b2f189fef
DWtb <carina92020915@gmail.com>
2022-11-21 16:13:13 +0800

    add email verify alert

---
+++ src/pages/email-verify/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { unionTypeAnnotation } from "@babel/types";
+import Swal from "sweetalert2";
+
+useHead({ title: "驗證 | UniCourse" });
+
+const route = useRoute();
+const router = useRouter();
+const processing = ref(false);
+
+async function verified() {
+    if (processing.value) {
+        return;
+    }
+    processing.value = true;
+    Swal.fire({
+        icon: "success",
+        title: "驗證成功",
+        text: `註冊信箱驗證成功！`,
+    });
+}
+verified();
+
+router.push("/");
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style scoped></style>
