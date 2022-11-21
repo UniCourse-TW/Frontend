5e4c1853ce09e0bfd5854438ae0c197a5086cda9
DWtb <carina92020915@gmail.com>
2022-11-21 10:50:15 +0800

    Merge remote-tracking branch 'origin/feat-add-client' into me

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import uni from "../../uni";
 const route = useRoute();
 const router = useRouter();
 
@@ -32,12 +33,7 @@ async function login() {
     processing.value = true;
 
     console.log(l_username.value, l_password.value);
-
-    await new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(true);
-        }, 1000);
-    });
+    await uni.login(l_username.value, l_password.value);
 
     processing.value = false;
 }
@@ -72,9 +68,8 @@ async function register() {
                     <Input label="帳號" placeholder="請輸入帳號" v-model="l_username" />
                     <Input label="密碼" placeholder="請輸入密碼" type="password" v-model="l_password" />
                     <div class="my-2 h-4 w-full">
-                        <Link to="/auth/reset"
-                            class="float-right cursor-pointer text-blue-500 transition-all hover:text-fuchsia-500">
-                        忘記密碼？
+                        <Link to="/auth/reset" class="float-right cursor-pointer text-blue-500 transition-all hover:text-fuchsia-500">
+                            忘記密碼？
                         </Link>
                     </div>
                 </div>
@@ -83,7 +78,6 @@ async function register() {
                     <Input label="密碼" placeholder="請輸入密碼" type="password" v-model="r_password" />
                     <Input label="電子郵件" placeholder="請輸入電子郵件" v-model="r_email" />
                     <Input label="邀請碼" placeholder="請輸入邀請碼" v-model="r_invite" />
-
                 </div>
             </transition>
 
@@ -91,7 +85,8 @@ async function register() {
                 <div class="mt-8 w-full text-center text-xl" :key="type">
                     <button
                         class="w-1/3 rounded bg-gradient-to-br from-cyan-500 via-indigo-500 to-fuchsia-500 p-2 px-4 text-white outline-none transition-all hover:hue-rotate-15 sm:w-1/4"
-                        @click="() => (type === 'login' ? login() : register())">
+                        @click="() => (type === 'login' ? login() : register())"
+                    >
                         {{ processing ? conv[type] + "中" : conv[type] }}
                     </button>
                 </div>
@@ -101,7 +96,8 @@ async function register() {
                 <div :key="type" class="mt-8 w-full text-center">
                     <span
                         class="text-rainbow cursor-pointer p-2 px-4 outline-none transition-all duration-200 hover:text-xl"
-                        @click="switch_type">
+                        @click="switch_type"
+                    >
                         {{ type === "login" ? "想要註冊嗎？" : "是要登入嗎？" }}
                     </span>
                 </div>
