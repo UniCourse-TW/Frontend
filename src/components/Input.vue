2981d6b4a64c7242f7c3a654dce1101f05634180
JacobLinCool <jacoblincool@gmail.com>
2022-11-24 02:34:55 +0800

    fix: input data binding

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -13,6 +13,8 @@ const emit = defineEmits(["update:modelValue"]);
             <input
                 class="m-2 w-full border-b-[3px] border-blue-300 p-2 text-indigo-500 outline-none transition-all duration-200 focus:border-indigo-500"
                 v-bind="$attrs"
+                @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+                v-bind:value="$attrs.modelValue || $attrs.value"
             />
         </label>
     </div>
