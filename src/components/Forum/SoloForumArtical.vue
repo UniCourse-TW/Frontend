80d26dc6fdc655d4e466bf1af14853ba08bcd7e6
DWtb <carina92020915@gmail.com>
2022-11-28 20:30:34 +0800

    fix some solo forum article type described

--- src/components/Forum/SoloForumArtical.vue
+++ src/components/Forum/SoloForumArtical.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { PropType } from "vue";
-import type { Post } from "../../types";
+import type { Post } from "@unicourse-tw/prisma";
 import { marked } from "marked";
 
 const props = defineProps({
@@ -17,34 +17,34 @@ const post = props.post;
     <div v-if="post">
         <div class="h-full w-full overflow-hidden rounded-t-2xl bg-white p-4">
             <div class="inline-block rounded bg-purple-700 px-1 py-0.5 text-sm text-white">
-                <div v-if="post.type == 'review'">
+                <div v-if="post.type == 'Article'">
                     <span>{{ "修課心得" }}</span>
                 </div>
-                <div v-else-if="post.type == 'question'">
+                <div v-else-if="post.type == 'Question'">
                     <span>{{ "提問" }}</span>
                 </div>
-                <div v-else-if="post.type == 'other'">
+                <div v-else-if="post.type == 'Other'">
                     <span>{{ "其他" }}</span>
                 </div>
             </div>
             <h1 class="my-1 text-xl font-bold">
-                <span>{{ post.course.name }}</span>
-                <span class="text-base">
+                <span>{{ post.course_id }}</span>
+                <!-- <span class="text-base">
                     <span class="text-gray-400"> | </span>
                     <span title="開課學年度" class="text-gray-600">{{ post.course.year }}</span>
                     <span class="text-gray-400">-</span>
                     <span title="開課學期" class="text-gray-600">{{ post.course.term }}</span>
                     <span class="text-gray-400"> | </span>
                     <span title="授課教師" class="text-gray-600">教師：{{ post.course.teacher }}</span>
-                </span>
+                </span> -->
             </h1>
 
-            <h2 class="my-1 text-sm text-gray-500">發布者：{{ post.author }}</h2>
-            <h2 class="my-1 text-sm text-gray-500">{{ post.time }}</h2>
+            <h2 class="my-1 text-sm text-gray-500">發布者：{{ post.author_id }}</h2>
+            <h2 class="my-1 text-sm text-gray-500">{{ post.updated }}</h2>
 
             <hr />
 
-            <div v-if="post.type == 'review'" class="mt-2 mb-2 grid grid-cols-[4rem_auto] text-gray-600">
+            <!-- <div v-if="post.type == 'review'" class="mt-2 mb-2 grid grid-cols-[4rem_auto] text-gray-600">
                 <span>甜度</span>
                 <div>
                     <IconStars :value="post.course.rating.sweetness" />
@@ -60,17 +60,17 @@ const post = props.post;
                     <IconStars :value="post.course.rating.usefulness" />
                     ( {{ post.course.rating.usefulness }} 分 )
                 </div>
-            </div>
+            </div> -->
             <h1 class="my-1 text-base font-bold text-gray-600">{{ post.title }}</h1>
             <div>{{ post.content }}</div>
-            <div class="relative mt-3 sm:mt-8">
+            <!-- <div class="relative mt-3 sm:mt-8">
                 <div class="absolute inset-y-0 right-0 inline-grid w-16 grid-cols-4 gap-1">
                     <img src="../../assets/icons/thumbs_up.svg" class="w-4" />
                     <h6 class="inset-y-0 right-0 text-xs text-purple-700">{{ post.vote.up }}</h6>
                     <img src="../../assets/icons/thumbs_down.svg" class="w-4" />
                     <h6 class="inset-y-0 right-0 text-xs text-purple-700">{{ post.vote.down }}</h6>
                 </div>
-            </div>
+            </div> -->
             <div class="h-5"></div>
         </div>
     </div>
